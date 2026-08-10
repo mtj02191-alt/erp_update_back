a5f7735cf81a9cf459dab957adba0c03678c25b1
razaoffi-droid <razaoffi@gmail.com>
2026-08-10 17:09:12 +0500

    update the ceo dashboard

--- src/app.module.ts
+++ src/app.module.ts
@@ -40,14 +40,14 @@ import { CeoOfficeModule } from "./ceo_office/ceo-office.module";
     }),
     TypeOrmModule.forRoot({
       type: "postgres",
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      // host: process.env.DB_HOST,
+      // port: parseInt(process.env.DB_PORT),
+      // username: process.env.DB_USERNAME,
+      // password: process.env.DB_PASSWORD,
+      // database: process.env.DB_NAME,
 
-      // url: process.env.DATABASE_URL || 'postgresql://postgres:postgres@localhost:5432/ddr_db',
-      // ssl: process.env.SSL === 'production',
+      url: process.env.DATABASE_URL || '${{ Postgres.DATABASE_PRIVATE_URL }}',
+      ssl: process.env.SSL === 'production',
       autoLoadEntities: true,
       synchronize: true,
       extra: {
